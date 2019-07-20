16cf37f9af4d911b9a4008deaef822df03f33112
Crysicia <lucienmontmartre@yahoo.fr>
2019-07-20 14:30:07 +0200

    👮‍♂️ What is your name, sir ?

---
+++ exo_9.rb
@@ -0,0 +1,11 @@
+print "Quel est ton prénom ?\n> "
+first_name = gets.chomp
+
+print "Quel est ton nom de famille ?\n> "
+last_name = gets.chomp
+
+puts "Bonjour #{first_name} #{last_name} !"
+
+# Alternative :
+# puts 'Quel est ton prénom ?'
+# print '> '
