11e55c9c43f9a6245cc24162d6c56d68c02c457c
Crysicia <lucienmontmartre@yahoo.fr>
2019-07-20 14:02:49 +0200

    📦 We store data !

---
+++ exo_6.rb
@@ -0,0 +1,10 @@
+# We define three variables
+# Variables are a way of storing data so we can reuse it elsewhere on our code
+number_of_hours_worked_per_day = 10
+number_of_days_worked_per_week = 5
+number_of_weeks_in_THP = 11
+
+puts "Travail : #{number_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_in_THP}"
+
+# The line below doesn't work because 'number_of_minutes_in_an_hour' is not defined.
+puts "Et en minutes ça fait : #{number_of_minutes_in_an_hour * number_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_in_THP}"
