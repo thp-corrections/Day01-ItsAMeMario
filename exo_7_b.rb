4f8ee3b7db9baf59d036239a73ba11c7d7ee3a9c
Crysicia <lucienmontmartre@yahoo.fr>
2019-07-20 14:23:01 +0200

    📩 Now with a cute prompt

---
+++ exo_7_b.rb
@@ -0,0 +1,6 @@
+puts "Bonjour, c'est quoi ton blase ?"
+
+# Since print doesn't add a trailing newline, we can ad characters in front of our user input
+print "> "
+user_name = gets.chomp
+puts user_name
