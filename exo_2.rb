2b6f9c0fa26f153fa0349579a88005a735017de6
Crysicia <lucienmontmartre@yahoo.fr>
2019-07-20 13:50:44 +0200

    🎤 And now with a sexy voice

---
+++ exo_2.rb
@@ -0,0 +1,12 @@
+puts <<-TEXT
+Bonjour, monde !
+Et avec une voix sexy, ça donne : Bonjour, monde !
+TEXT
+
+# Alternatives :
+# puts 'Bonjour, monde !\nEt avec une voix sexy, ça donne : Bonjour, monde !'
+# ---
+# print 'Bonjour, monde !\nEt avec une voix sexy, ça donne : Bonjour, monde !\n'
+# ---
+# puts 'Bonjour, monde !'
+# puts 'Et avec une voix sexy, ça donne : Bonjour, monde !'
